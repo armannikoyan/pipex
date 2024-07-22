7ad5f4b5e3b8f23356a1f87c3ca45cbdcca84342
Arman Nikoyan <armannikoyan04@icloud.com>
2024-07-22 14:08:20 +0400

    Done

--- pipex.c
+++ pipex.c
@@ -6,73 +6,86 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 14:15:39 by anikoyan          #+#    #+#             */
-/*   Updated: 2024/07/22 13:04:38 by anikoyan         ###   ########.fr       */
+/*   Updated: 2024/07/22 14:07:02 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	ft_child_process(char **data, int *fd, char **envpath)
+static void	ft_process_input(char *data, int *fd, int *fd_file, char **envpath)
 {
 	char	**envp;
 	char	**cmd;
 
-	dup2(fd[2], STDIN_FILENO);
-	close(fd[2]);
-	close(fd[3]);
+	close(fd_file[1]);
+	dup2(fd_file[0], STDIN_FILENO);
+	close(fd_file[0]);
 	close(fd[0]);
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[1]);
-	cmd = ft_split(data[1], ' ');
+	cmd = ft_split(data, ' ');
 	envp = ft_envpath(envpath);
 	cmd[0] = ft_path(cmd[0], envp);
 	if (execve(cmd[0], cmd, envp) == -1)
 		exit(EXIT_FAILURE);
+	ft_free_data(cmd);
 }
 
-static void	ft_parent_process(char **data, int *fd, char **envpath)
+static void	ft_process_output(char *data, int *fd, int *fd_file, char **envpath)
 {
 	char	**envp;
 	char	**cmd;
 
-	dup2(fd[3], STDOUT_FILENO);
-	close(fd[2]);
-	close(fd[3]);
+	close(fd_file[0]);
+	dup2(fd_file[1], STDOUT_FILENO);
+	close(fd_file[1]);
 	close(fd[1]);
 	dup2(fd[0], STDIN_FILENO);
 	close(fd[0]);
-	cmd = ft_split(data[2], ' ');
+	cmd = ft_split(data, ' ');
 	envp = ft_envpath(envpath);
 	cmd[0] = ft_path(cmd[0], envp);
 	if (execve(cmd[0], cmd, envp) == -1)
 		exit(EXIT_FAILURE);
+	ft_free_data(cmd);
+}
+
+static void	ft_25_line_rule(int *fd, int *fd_file, pid_t *pid, char **data)
+{
+	close(fd[0]);
+	close(fd[1]);
+	close(fd_file[0]);
+	close(fd_file[1]);
+	waitpid(pid[0], NULL, 0);
+	waitpid(pid[1], NULL, 0);
+	ft_free_data(data);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	**data;
-	int		fd[4];
-	pid_t	pid;
+	int		fd[2];
+	int		fd_file[2];
+	pid_t	pid[2];
 
 	if (argc != 5 || pipe(fd) == -1)
 		return (EXIT_FAILURE);
 	data = ft_format_argv(argc, argv);
-	fd[2] = open(data[0], O_RDONLY);
-	fd[3] = open(data[argc - 2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd[2] == -1 || fd[3] == -1)
+	fd_file[0] = open(data[0], O_RDONLY);
+	fd_file[1] = open(data[argc - 2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_file[0] == -1 || fd_file[1] == -1)
 		exit(EXIT_FAILURE);
-	pid = fork();
-	if (pid == -1)
+	pid[0] = fork();
+	if (pid[0] == -1)
 		return (EXIT_FAILURE);
-	if (pid == 0)
-		ft_child_process(data, fd, envp);
-	else
-		ft_parent_process(data, fd, envp);
-	waitpid(pid, NULL, 0);
-	close(fd[0]);
-	close(fd[1]);
-	close(fd[2]);
-	close(fd[3]);
-	ft_free_data(data);
+	if (pid[0] > 0)
+		pid[1] = fork();
+	if (pid[1] == -1)
+		return (EXIT_FAILURE);
+	if (pid[0] == 0)
+		ft_process_input(data[1], fd, fd_file, envp);
+	if (pid[1] == 0)
+		ft_process_output(data[2], fd, fd_file, envp);
+	ft_25_line_rule(fd, fd_file, pid, data);
 	return (EXIT_SUCCESS);
 }
